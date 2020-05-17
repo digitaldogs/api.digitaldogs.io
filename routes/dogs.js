b410499ad74c73434ed4601450d434cafcc3b686
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2020-05-17 13:22:11 +1000

    Clean up

--- routes/dogs.js
+++ routes/dogs.js
@@ -62,42 +62,6 @@ const pack_abi = [
   }
 ]
 
-  // name: "_pack",
-  // outputs: [
-  //   {
-  //     name: "name",
-  //     type: "string"
-  //   },
-  //   {
-  //     name: "dob",
-  //     type: "uint256"
-  //   },
-  //   {
-  //     name: "microchip",
-  //     type: "string"
-  //   },
-  //   {
-  //     name: "dam",
-  //     type: "uint256"
-  //   },
-  //   {
-  //     name: "sire",
-  //     type: "uint256"
-  //   },
-  //   {
-  //     name: "sex",
-  //     type: "uint8"
-  //   },
-  //   {
-  //     name: "timestamp",
-  //     type: "uint256"
-  //   }
-  // ],
-  // payable: false,
-  // stateMutability: "view",
-  // type: "function"
-
-
 const add_abi = [
   {
     "constant": true,
@@ -175,7 +139,7 @@ router.get("/count", (req, res) => {
   });
 });
 
-router.get("/pack", (req, res) => {
+router.get("/pack/:id", (req, res) => {
 
   const key = process.env.INFURA_KEY;
 
@@ -183,38 +147,22 @@ router.get("/pack", (req, res) => {
   const eth = new Eth(new Eth.HttpProvider("https://mainnet.infura.io/v3/eaf5e0b4a01042a48211762c8d4eec44"));
   const contract = eth.contract(pack_abi).at("0x4390282c7d623edee9aacb971303077aba2d5e14");
   
-  contract.pack(0).then((pack) => {
+  contract.pack(req.params.id).then((pack) => {
     res.send(pack);
   });
 });
 
 router.get("/:id", (req, res) => {
-  res.send("Get individual dog details");
-});
+  const key = process.env.INFURA_KEY;
+  console.log(key);
 
-router.get("/:id/pedigree", (req, res) => {
-  res.send(
-    JSON.stringify([
-      {
-        owner: 123,
-        name: "fido",
-        sex: "male",
-        microchip: "abcde123",
-        dob: "2018-01-01",
-        sire: "557678",
-        dam: "45023"
-      },
-      {
-        owner: 456,
-        name: "Ralph",
-        sex: "female",
-        microchip: "asdkjlkln2",
-        dob: "2017-01-01",
-        sire: "8815465",
-        dam: "328668"
-      }
-    ])
-  );
+  //const eth = new Eth(new Eth.HttpProvider(node + key));
+  const eth = new Eth(new Eth.HttpProvider("https://mainnet.infura.io/v3/" + key));
+  const contract = eth.contract(pack_abi).at("0x4390282c7d623edee9aacb971303077aba2d5e14");
+  
+  contract.pack(req.params.id).then((pup) => {
+    res.send(pup);
+  });
 });
 
 router.post("/", (req, res) => {
